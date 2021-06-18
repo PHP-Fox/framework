553609d55777c2b2b44457db588e1cbe3c1dd72e
Steve McDougall <juststevemcd@gmail.com>
2021-06-18 10:24:51 +0100

    Moving example to directory to test local php servert

--- examples/index.php
+++ examples/index.php
@@ -5,10 +5,10 @@ declare(strict_types=1);
 
 use PHPFox\Application;
 
-require __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
 
 $app = Application::boot(
-    basePath: __DIR__ . '/',
+    basePath: __DIR__ . '/../',
 );
 
 $app->run();
